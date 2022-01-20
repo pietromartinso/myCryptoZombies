1673acc8f8a42566647acf9016b97d0e5f9dc2cc
pietromartins <pietromartinso@gmail.com>
2022-01-20 09:28:07 -0300

    Just finished second lesson

---
+++ Lesson 2/ZombieFeeding.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.4.19;
+
+import "./zombiefactory.sol";
+
+contract KittyInterface {
+    function getKitty(uint256 _id)
+        external
+        view
+        returns (
+            bool isGestating,
+            bool isReady,
+            uint256 cooldownIndex,
+            uint256 nextActionAt,
+            uint256 siringWithId,
+            uint256 birthTime,
+            uint256 matronId,
+            uint256 sireId,
+            uint256 generation,
+            uint256 genes
+        );
+}
+
+contract ZombieFeeding is ZombieFactory {
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(ckAddress);
+
+    // Modifique a função aqui:
+    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+        _targetDna = _targetDna % dnaModulus;
+        uint256 newDna = (myZombie.dna + _targetDna) / 2;
+        // Adicione a declaração de `if` aqui
+        _createZombie("NoName", newDna);
+    }
+
+    function feedOnKitty(uint256 _zombieId, uint256 _kittyId) public {
+        uint256 kittyDna;
+        (, , , , , , , , , kittyDna) = kittyContract.getKitty(_kittyId);
+        // E modifique a função aqui:
+        feedAndMultiply(_zombieId, kittyDna);
+    }
+}
