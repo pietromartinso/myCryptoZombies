577a7f71169ff3ab399bac30f4afc3fcab030a1b
pietromartins <pietromartinso@gmail.com>
2022-01-20 11:22:11 -0300

    Testing - repository renamed

--- Lesson 3/Ownable.sol
+++ Lesson 3/Ownable.sol
@@ -34,6 +34,6 @@ contract Ownable {
     function transferOwnership(address newOwner) public onlyOwner {
         require(newOwner != address(0));
         OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
+        owner = newOwner; //test
     }
 }
